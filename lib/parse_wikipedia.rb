603f4ad9dc31998f6706364220fa335f7474d491
dmlaziuk <dm.laziuk@gmail.com>
2017-11-06 23:47:25 +0300

    wikipedia parse wikitext

--- lib/parse_wikipedia.rb
+++ lib/parse_wikipedia.rb
@@ -5,7 +5,7 @@ require 'mechanize'
 class ParseWikipedia
   WIKI_PAGE = 'https://en.wikipedia.org/wiki/List_of_gay,_lesbian_or_bisexual_people'.freeze
   WIKI_LINKS = '//*[@id="mw-content-text"]/div/div[3]/ul/li/a'.freeze
-  WIKI_QUERY = 'https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=revisions&rvprop=content&titles='.freeze
+  WIKI_TABLE = '//*[@id="mw-content-text"]/div/table[2]/tr'.freeze
   ACTORS = 'actors:wiki'.freeze
 
   def initialize
@@ -15,10 +15,30 @@ class ParseWikipedia
 
   def run
     links = @agent.get(WIKI_PAGE).links_with(xpath: WIKI_LINKS)
-    hrefs = links.map { |link| link.href[6..-1] }
-    ll = WIKI_QUERY + hrefs.join('|')
-    data = JSON.parse(@agent.get(ll).content)
-    text = data['query']['pages'].map { |p| p['revisions'].first['content'] }
-    puts text
+    page = @agent.get('https://en.wikipedia.org/w/index.php?title=List_of_gay,_lesbian_or_bisexual_people:_A&action=edit')
+    txt =  page.css('#wpTextbox1').text
+    txt = txt.split('{| class="wikitable sortable"').last
+    txt = txt.split('|}').first
+    txt = txt.split('|- valign="top"').last
+    txt = txt.split("|-\n")
+    txt.each(&:strip!)
+    txt.each_with_index do |item, index|
+      arr = item.split("\n")
+      next if arr[0].nil?
+      arr[0].gsub!('{', '')
+      arr[0].gsub!('}', '')
+      arr[0].gsub!('[', '')
+      arr[0].gsub!(']', '')
+      arr[0].gsub!('sortname|', '')
+      arr[0].gsub!('sort|', '')
+      names = arr[0][2..-1].split('|')
+      print "#{index}: "
+      names.each_with_index do |item, index|
+        item.gsub!(/\(.*?\)/, '')
+        item.strip!
+        print "[#{index}]#{item}"
+      end
+      puts "|\n"
+    end
   end
 end
